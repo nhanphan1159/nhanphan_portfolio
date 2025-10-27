d7c8fcf0b1e0ec5c39677069fc238f6aa059f0a1
teq-nhanphan <nhanphan@teqnological.asia>
2025-10-27 14:49:32 +0700

    feat: init source

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    // ...,
+  ],
+});
